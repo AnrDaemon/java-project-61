a72ba28902e1973cece5c3944aad47f64e3c534b
AnrDaemon <anrdaemon@yandex.ru>
2024-06-19 23:08:40 +0300

    Improved project structure

--- src/main/java/io/hexlet/code/App.java
+++ src/main/java/io/hexlet/code/App.java
@@ -1,19 +1,59 @@
 package io.hexlet.code;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
+
+    private String userName = "stranger";
+
+    private Map<Integer, GameInterface> games;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
     public String getGreeting() {
-        return "Welcome to the Brain Games!";
+        return "Welcome to the Brain Games, %s!";
     }
 
     public String getNameRequest() {
-        return "May I have your name? ";
+        return "May I have your name?";
     }
 
-    public String getWelcomeString(String userName) {
-        return "Hello, " + userName + "!";
+    public String getWelcomeString() {
+        return "Hello, %s! Glad to meet you!";
+    }
+
+    public String getSelectionPrompt() {
+        return "Please select your game:";
+    }
+
+    public void run() {
+        games = new HashMap<Integer, GameInterface>();
+        games.put(0, null);
+        games.put(1, new Greeting());
+
+        int n = 1;
+        while (n > 0) {
+            games.get(n).play(this);
+
+            int length = games.size();
+            for (int i = 1; i < length; i++) {
+                Cli.println("%d. %s", i, games.get(i).getName());
+            }
+
+            Cli.println("%d. %s", 0, "Exit");
+            n = Integer.parseInt(Cli.read(this.getSelectionPrompt()));
+        }
     }
 
     public static void main(String[] args) {
-        Cli.greeting(new App());
+        App app = new App();
+        app.run();
     }
 }
