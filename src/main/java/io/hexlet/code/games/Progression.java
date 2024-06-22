b71a76e46e91fe313d9851742374a9ac381ac363
AnrDaemon <anrdaemon@yandex.ru>
2024-06-22 21:50:05 +0300

    Stage 8: Added Progression game (#7)

---
+++ src/main/java/io/hexlet/code/games/Progression.java
@@ -0,0 +1,45 @@
+package io.hexlet.code.games;
+
+import java.util.Random;
+
+import io.hexlet.code.App;
+import io.hexlet.code.GameInterface;
+import io.hexlet.code.GameRound;
+
+public class Progression implements GameInterface {
+
+    private final String name = "Progression";
+
+    private Random rand;
+
+    public Progression(App app) {
+        this.rand = new Random();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getRules() {
+        return "Determine and enter the missing member of arithmetic progression.";
+    }
+
+    @Override
+    public GameRound play() {
+        int n1 = rand.nextInt(20) + 1;
+        int step = rand.nextInt(10) + 1;
+        int length = rand.nextInt(4, 8) + 1;
+        int x = rand.nextInt(length);
+        String a = Integer.toString(n1 + step * x);
+        String q = "";
+        for (int i = 0; i < length; i++) {
+            q += (q.length() > 0 ? " " : "") + (i == x ? ".." : n1);
+            n1 += step;
+        }
+
+        return new GameRound(q, a);
+    }
+
+}
